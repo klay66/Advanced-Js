955e5ec494726dc64d883bfccbf97e902f45bd2e
klay66 <klaytop112233@gmail.com>
2024-08-25 13:45:22 +0300

    regular rxbression

--- index.js
+++ index.js
@@ -312,7 +312,158 @@
 // console.log({ ...objOne, ...objTwo, e: 5 });
 
 
-let n1 = [10, 30, 10, 20];
-let n2 = [30, 20, 10];
+// let n1 = [10, 30, 10, 20];
+// let n2 = [30, 20, 10];
 
-console.log(Math.max(...n2) * (n1.concat(n2).length))
+// console.log(Math.max(...n2) * (n1.concat(n2).length))
+
+
+////////regular exbression
+
+// let myString = 'Hello Elzero Web School I Love elzero'
+
+// let ragx = /Elzero/ig
+
+// console.log(myString.match(ragx));
+
+// ranges
+
+// let tld = "Com Net Org Info Code Io";
+// let tldRg = /(com | info | io)/ig
+// console.log(tld.match(tldRg));
+
+
+// let nums = "12345678910";
+// let numsRe = /[0-9]/g
+// console.log(nums.match(numsRe));
+
+// let notNums = "12345678910";
+// let notNsRe = /[^0-2]/g
+// console.log(notNums.match(notNsRe));
+
+// let specialNums = "1!2@3#4$5%678910";
+// let specialNumsRe = /[^0-9]/g;
+// console.log(specialNums.match(specialNumsRe));
+
+
+// let practice = "Os1 Os1Os Os2 Os8 Os8Os";
+// let practiceRe = /os[5-9]os/ig
+// console.log(practice.match(practiceRe));
+
+
+// let myString = "AaBbcdefG123!234%^&*";
+
+// let atozSmall = /[a-z]/g;
+// let NotAtozSmall = /[^a-z]/g;
+// let atozCapital = /[A-Z]/g;
+// let NotAtozCapital = /[^A-Z]/g;
+// let aAndcAnde = /[ace]/g;
+// let NotaAndcAnde = /[^ace]/g;
+// let lettersCapsAndSmall = /[a-z]/ig
+// let notLettersCapsAndSmall = /[^a-z]/ig
+// let specials = /[^a-zA-Z0-9]/g;
+
+// console.log(myString.match(atozSmall));
+// console.log(myString.match(NotAtozSmall));
+// console.log(myString.match(atozCapital));
+// console.log(myString.match(NotAtozCapital));
+// console.log(myString.match(aAndcAnde));
+// console.log(myString.match(NotaAndcAnde));
+// console.log(myString.match(lettersCapsAndSmall));
+// console.log(myString.match(notLettersCapsAndSmall));
+// console.log(myString.match(specials));
+
+
+// let email = 'O@@@g...com O@g.com O@g.net A@Y.com O-g.com o@s.org 1@1.com';
+// let dot = /./g;
+// let word = /\w/g;
+// let valid = /\w@\w.(com|net)/g;
+// console.log(email.match(dot));
+// console.log(email.match(word));
+// console.log(email.match(valid));
+
+
+
+// let names = "Sayed 1Spam 2Spam 3Spam Spam4 Spam5 Osama Ahmed Aspamo";
+// let re = /(\bspam|spam\b)/ig;
+// console.log(names.match(re));
+
+// console.log(re.test(names));
+// console.log(/(\bspam|spam\b)/ig.test("Osama"));
+// console.log(/(\bspam|spam\b)/ig.test("1Spam"));
+// console.log(/(\bspam|spam\b)/ig.test("Spam1"));
+
+
+// let mails = "o@nn.sa osama@gmail.com elzero@gmail.net osama@mail.ru"; // All Emails
+// // let mailsRe = /\w+@\w+.(com|net)/ig;
+// let mailsRe = /\w+@\w+.\w+/ig;
+// console.log(mails.match(mailsRe));
+
+// let nums = "0110 10 150 05120 0560 350 00"; // 0 Numbers Or No 0
+// let numsRe = /0\d*0/ig;
+// console.log(nums.match(numsRe));
+
+// let urls = "https://google.com http://www.website.net web.com"; // http + https
+// let urlsRe = /(https?:\/\/)?(www.)?\w+.\w+/ig;
+// console.log(urls.match(urlsRe));
+
+
+
+
+// let serials = "S100S S3000S S50000S S950000S";
+
+// console.log(serials.match(/s\d{3}s/ig)); // S[Three Number]S
+// console.log(serials.match(/s\d{4,5}s/ig)); // S[Four Or Five Number]S
+// console.log(serials.match(/s\d{4,}s/ig)); // S[At Least Four]S
+
+
+
+// let myString = "We Love Programming";
+// let names = "1OsamaZ 2AhmedZ 3Mohammed 4MoustafaZ 5GamalZ";
+
+// console.log(/ing$/ig.test(myString));
+// console.log(/^we/ig.test(myString));
+// console.log(/lz$/ig.test(names));
+// console.log(/^\d/ig.test(names));
+
+// console.log(names.match(/\d\w{5}(?=z)/ig));
+// console.log(names.match(/\d\w{8}(?!z)/ig));
+
+
+// let txt = "We Love Programming And @ Because @ Is Amazing";
+// console.log(txt.replace("@", "JavaScript"));
+// console.log(txt.replaceAll("@", "JavaScript"));
+// let re = /@/ig;
+// console.log(txt.replaceAll(re, "JavaScript"));
+// console.log(txt.replaceAll(/@/ig, "JavaScript"));
+
+// document.getElementById("register").onsubmit = function () {
+//     letphoneInput = document.getElementById('phone').value;
+//     let phoneRe = /\(\d{4}\)\s\d{3}-\d{4}/ig;
+//     let validationResult = phoneRe.test(letphoneInput);
+//     console.log(validationResult);
+//     if (validationResult === false) {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
+
+// let urls = "https://google.com http://www.website.net web.com"; // http + https
+// let urlsRe = /(https?:\/\/)?(www.)?\w+.\w+/ig;
+// console.log(urls.match(urlsRe));
+
+let url1 = 'elzero.org';
+let url2 = 'http://elzero.org';
+let url3 = 'https://elzero.org';
+let url4 = 'https://www.elzero.org';
+let url5 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics';
+
+let re = /(https?:\/\/)?(www.)?\w+.org(:\d{4})?(\S+)?/ig;
+
+console.log(url1.match(re));
+console.log(url2.match(re));
+console.log(url3.match(re));
+console.log(url4.match(re));
+console.log(url5.match(re));
