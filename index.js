ce5e07032587f190bebb268ca5dadcaa9d3b641c
klay66 <klaytop112233@gmail.com>
2024-08-27 13:28:55 +0300

    Date

--- index.js
+++ index.js
@@ -627,20 +627,322 @@
 // console.log(adminOne.sayHello());
 
 
-class User {
-    #e;
-    constructor(id, username, salary) {
-        this.i = id;
-        this.u = username;
-        this.#e = salary;
+// class User {
+//     #e;
+//     constructor(id, username, salary) {
+//         this.i = id;
+//         this.u = username;
+//         this.#e = salary;
 
-    }
-    getSalary() {
-        return parseInt(this.#e);
-    }
+//     }
+//     getSalary() {
+//         return parseInt(this.#e);
+//     }
+// }
+
+// let userOne = new User(100, "Elzero", "5000 Gneh");
+
+// console.log(userOne.u);
+// console.log(userOne.getSalary() * 0.3);
+
+/*
+  Prototype
+  - Introduction
+  - Prototypes are the mechanism by which JavaScript objects
+    inherit features from one another.
+*/
+
+// class User {
+//     constructor(id, username) {
+//         this.i = id;
+//         this.u = username;
+//     }
+//     sayHello() {
+//         return `Hello ${this.u}`;
+//     }
+// }
+
+// let userOne = new User(100, "Elzero");
+// console.log(userOne.u);
+
+// console.log(User.prototype);
+
+// let strOne = "Elzero";
+
+// console.log(String.prototype);
+
+
+
+// class User {
+//     constructor(id, username) {
+//         this.i = id;
+//         this.u = username;
+//     }
+//     sayHello() {
+//         return `Hello ${this.u}`;
+//     }
+// }
+
+// let userOne = new User(100, "Elzero");
+// console.log(userOne.u);
+// console.log(User.prototype);
+// console.log(userOne);
+
+// User.prototype.sayWelcome = function () {
+//     return `Welcome ${this.u}`;
+// };
+
+// Object.prototype.love = "Elzero Web School";
+
+// String.prototype.addDotBeforeAndAfter = function (val) {
+//     return `.${this}.`;
+// };
+
+// let myString = "Elzero";
+
+
+
+/*
+  Object Meta Data And Descriptor
+  - writable
+  - enumerable
+  - configurable [Cannot Delete Or Reconfigure]
+*/
+
+// const myObject = {
+//     a: 1,
+//     b: 2,
+// };
+
+// Object.defineProperty(myObject, "c", {
+//     writable: false,
+//     enumerable: true,
+//     configurable: false,
+//     value: 3,
+// });
+
+// // Object.defineProperty(myObject, "c", {
+// //   writable: false,
+// //   enumerable: true,
+// //   configurable: true, <= Cannot redefine property
+// //   value: 3,
+// // });
+
+// myObject.c = 100;
+
+// console.log(delete myObject.c);
+
+// for (let prop in myObject) {
+//     console.log(prop, myObject[prop]);
+// }
+
+// console.log(myObject);
+
+
+/*
+  Object Meta Data And Descriptor
+  - Define Multiple Properties
+  - Check Descriptors
+*/
+
+// const myObject = {
+//     a: 1,
+//     b: 2,
+// };
+
+// Object.defineProperties(myObject, {
+//     c: {
+//         configurable: true,
+//         value: 3,
+//     },
+//     d: {
+//         configurable: true,
+//         value: 4,
+//     },
+//     e: {
+//         configurable: true,
+//         value: 5,
+//     },
+// });
+
+// console.log(myObject);
+
+// console.log(Object.getOwnPropertyDescriptor(myObject, "d"));
+// console.log(Object.getOwnPropertyDescriptors(myObject));
+
+
+//////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\
+/*
+  Date And Time
+  - Date Constructor
+
+  Static Methods
+  - Date.now()
+
+  - To Track Time You Need Starting Point
+  - Epoch Time Or Unix Time In Computer Science Is The Number of Seconds Since January 1, 1970.
+  - Why 1970 [829 Days To 136 Years]
+
+  Search For
+  - Year 2038 Problem in Computer Science.
+*/
+
+
+// let dateNow = new Date()
+
+// console.log(dateNow);
+
+// console.log(Date.now());
+
+// let seconds = Date.now() / 1000
+// console.log(`Seconds ${seconds}`);
+
+// let minutes = seconds / 60; // Number Of Minutes
+// console.log(`Minutes ${minutes}`);
+
+// let hours = minutes / 60; // Number Of Hours
+// console.log(`Hours ${hours}`);
+
+// let days = hours / 24; // Number Of Days
+// console.log(`Days ${days}`);
+
+// let years = days / 365; // Number Of Years
+// console.log((`Years ${years}`));
+
+
+/*
+  Date And Time
+  - getTime() => Number Of Milliseconds
+  - getDate() => Day Of The Month
+  - getFullYear()
+  - getMonth() => Zero Based
+  - getDay() => Day Of The Week
+  - getHours()
+  - getMinutes()
+  - getSeconds()
+*/
+
+// let dateNow = new Date();
+// let birthday = new Date("jul 17, 2005");
+// let dateDiff = dateNow - birthday;
+
+// console.log(dateDiff);
+// console.log(dateDiff / 1000 / 60 / 60 / 24 / 365);
+
+// console.log(dateNow);
+// console.log(dateNow.getTime());
+// console.log(dateNow.getDate());
+// console.log(dateNow.getFullYear());
+// console.log(dateNow.getMonth());
+// console.log(dateNow.getDay());
+// console.log(dateNow.getHours());
+// console.log(dateNow.getMinutes());
+// console.log(dateNow.getSeconds());
+
+
+
+/*
+  Date And Time
+  - setTime(Milliseconds)
+  - setDate() => Day Of The Month [Negative And Positive]
+  - setFullYear(year, month => Optional [0-11], day => Optional [1-31])
+  - setMonth(Month [0-11], Day => Optional [1-31]) [Negative And Positive]
+  - setHours(Hours [0-23], Minutes => Optional [0-59], Seconds => Optional [0-59], MS => Optional [0-999])
+  - setMinutes(Minutes [0-59], Seconds => Optional [0-59], MS => Optional [0-999])
+  - setSeconds(Seconds => [0-59], MS => Optional [0-999])
+*/
+
+// let dateNow = new Date();
+// console.log(dateNow);
+
+// console.log("#".repeat(66));
+
+// // dateNow.setTime(0);
+// // console.log(dateNow);
+
+// // console.log("#".repeat(66));
+
+// // dateNow.setTime(10000);
+// // console.log(dateNow);
+
+// // console.log("#".repeat(66));
+
+// // dateNow.setDate(35);
+// // console.log(dateNow);
+
+// // dateNow.setFullYear(2020, 13);
+// // console.log(dateNow);
+
+// // dateNow.setMonth(15);
+// // console.log(dateNow);
+
+// dateNow.setHours(15);
+// console.log(dateNow);
+
+
+
+
+/*
+  Date And Time
+
+  new Date(timestamp)
+  new Date(Date String)
+  new Date(Numeric Values)
+
+  Format
+  - "Oct 25 1982"
+  - "10/25/1982"
+  - "1982-10-25" => ISO International Standard
+  - "1982 10"
+  - "1982"
+  - "82"
+  - 1982, 9, 25, 2, 10, 0
+  - 1982, 9, 25
+  - "1982-10-25T06:10:00Z"
+
+  Date.parse("String") // Read Date From A String
+*/
+
+// console.log(Date.parse("Oct 25 1982"));
+
+// let date1 = new Date(0);
+// console.log(date1);
+
+// let date2 = new Date(404344800000);
+// console.log(date2);
+
+// let date3 = new Date("10-25-1982");
+// console.log(date3);
+
+// let date4 = new Date("1982-10-25");
+// console.log(date4);
+
+// let date5 = new Date("1982-10");
+// console.log(date5);
+
+// let date6 = new Date("82");
+// console.log(date6);
+
+// let date7 = new Date(1982, 9, 25, 2, 10, 0);
+// console.log(date7);
+
+// let date8 = new Date(1982, 9, 25);
+// console.log(date8);
+
+// let date9 = new Date("1982-10-25T06:10:00z");
+// console.log(date9);
+
+
+
+let start = new Date()
+
+
+for (i = 0; i < 1000; i++) {
+    document.write(`<div>${i}</div>`)
 }
 
-let userOne = new User(100, "Elzero", "5000 Gneh");
 
-console.log(userOne.u);
-console.log(userOne.getSalary() * 0.3);
+let end = new Date()
+let duration = end - start;
+
+console.log(duration);
