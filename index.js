b5035c5d8fda84bf292a3e1fc1ddab369c4d1cb3
klay66 <klaytop112233@gmail.com>
2024-08-29 16:52:06 +0300

    promise and fatch and async

--- index.js
+++ index.js
@@ -1138,3 +1138,484 @@
 // console.log("2");
 // setTimeout(() => console.log("Operation"), 3000);
 // console.log("3");
+
+/*
+  To Understand Ajax, Fetch, Promises
+
+  Call Stack || Stack Trace
+  -- JavaScript Engine Uses A Call Stack To Manage Execution Contexts
+  -- Mechanism To Make The Interpreter Track Your Calls
+  -- When Function Called It Added To The Stack
+  -- When Function Executed It Removed From The Stack
+  -- After Function Is Finished Executing The Interpreter Continue From The Last Point
+  -- Work Using LIFO Principle => Last In First Out
+  -- Code Execution Is Synchronous.
+  -- Call Stack Detect Web API Methods And Leave It To The Browser To Handle It
+
+  Web API
+  -- Methods Available From The Environment => Browser
+*/
+
+// setTimeout(() => {
+//   console.log("Web API");
+// }, 0);
+
+// function one() {
+//   console.log("One");
+// }
+// function two() {
+//   one();
+//   console.log("Two");
+// }
+// function three() {
+//   two();
+//   console.log("Three");
+// }
+
+// three();
+
+// /*
+// =================================
+// console.log("One");
+// =================================
+// function one() {
+//   console.log("One");
+// }
+// =================================
+// function two() {
+//   one();
+//   console.log("Two");
+// }
+// =================================
+// function three() {
+//   two();
+//   console.log("Three");
+// }
+// =================================
+// */
+
+// console.log("#####");
+// console.log("One");
+// console.log("Two");
+// console.log("Three");
+
+
+/*
+  To Understand Ajax, Fetch, Promises
+
+  Event Loop + Callback Queue
+
+  Story
+  - JavaScript Is A Single Threaded Language "All Operations Executed in Single Thread"
+  - Call Stack Track All Calls
+  - Every Function Is Done Its Poped Out
+  - When You Call Asynchronous Function It Sent To Browser API
+  - Asynchronous Function Like Settimeout Start Its Own Thread
+  - Browser API Act As A Second Thread
+  - API Finish Waiting And Send Back The Function For Processing
+  - Browser API Add The Callback To Callback Queue
+  - Event Loop Wait For Call Stack To Be Empty
+  - Event Loop Get Callback From Callback Queue And Add It To Call Stack
+  - Callback Queue Follow FIFO "First In First Out" Rule
+*/
+
+// console.log("One");
+// setTimeout(() => {
+//   console.log("Three");
+// }, 0);
+// setTimeout(() => {
+//   console.log("Four");
+// }, 0);
+// console.log("Two");
+
+// setTimeout(() => {
+//   console.log(myVar);
+// }, 0);
+
+// let myVar = 100;
+// myVar += 100;
+
+
+/*
+  AJAX
+  - Asynchronous JavaScript And XML
+  - Approach To Use Many Technologies Together [HTML, CSS, Js, DOM]
+  - It Use "XMLHttpRequest" Object To Interact With The Server
+  - You Can Fetch Data Or Send Data Without Page Refresh
+  - Examples
+  --- Youtube Studio
+  --- Google Drive
+  --- Upload Article Photo
+  --- Form Check Name
+
+  Test new XMLHttpRequest();
+  Request And Response
+  Status Code
+*/
+
+// let req = new XMLHttpRequest()
+// console.log(req);
+
+
+/*
+  Ajax
+  - Ready State => Status Of The Request
+  [0] Request Not Initialized
+  [1] Server Connection Established
+  [2] Request Received
+  [3] Processing Request
+  [4] Request Is Finished And Response Is Ready
+  - Status
+  [200] Response Is Successful
+  [404] Not Found
+*/
+
+// let myReq = new XMLHttpRequest();
+
+// myReq.open("GET", "https://api.github.com/users/klay66/repos")
+// myReq.send()
+// console.log(myReq);
+
+// myReq.onreadystatechange = function () {
+//   // console.log(myReq.readyState);
+//   // console.log(myReq.status);
+//   if (this.readyState === 4 && this.status === 200) {
+//     console.log(this.responseText);
+//     let jsData = JSON.parse(this.responseText)
+//     for (let i = 0; i < jsData.length; i++) {
+//       let div = document.createElement("div")
+//       let repoName = document.createTextNode(jsData[i].name)
+//       div.appendChild(repoName)
+//       document.body.appendChild(div)
+//     }
+//   }
+
+// }
+
+
+// function make(e) {
+//   e.target.style.color = "#123098"
+// }
+
+// let p = document.querySelector(".text")
+// p.addEventListener("click", make)
+
+// function iamACallback() {
+//   console.log("Iam A Callback Function");
+// }
+
+// setTimeout(iamACallback, 2000);
+
+// setTimeout(() => {
+//   console.log("Download Photo From URL");
+//   setTimeout(() => {
+//     console.log("Resize Photo");
+//     setTimeout(() => {
+//       console.log("Add Logo To The Photo");
+//       setTimeout(() => {
+//         console.log("Show The Photo In Website");
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+/*
+ Promise Intro And Syntax
+  - Promise In JavaScript Is Like Promise In Real Life
+    - Promise Is Something That Will Happen In The Future
+      - Promise Avoid Callback Hell
+        - Promise Is The Object That Represent The Status Of An Asynchronous Operation And Its Resulting Value
+
+          - Promise Status
+--- Pending: Initial State
+--- Fulfilled: Completed Successfully
+--- Rejected: Failed
+
+Story
+  - Once A Promise Has Been Called, It Will Start In A Pending State
+    - The Created Promise Will Eventually End In A Resolved State Or In A Rejected State
+      - Calling The Callback Functions(Passed To Then And Catch) Upon Finishing.
+
+  - Then
+--- Takes 2 Optional Arguments[Callback For Success Or Failure]
+*/
+
+// const myPromise = new Promise((resolveFunction, rejectFunction) => {
+//   let connect = false;
+//   if (connect) {
+//     resolveFunction("Connection Established");
+//   } else {
+//     rejectFunction(Error("Connection Failed"));
+//   }
+// }).then(
+//   (resolveValue) => console.log(`Good ${resolveValue}`),
+//   (rejectValue) => console.log(`Bad ${rejectValue}`)
+// );
+
+// const myPromise = new Promise((resolveFunction, rejectFunction) => {
+//   let connect = true
+//   if (connect) {
+//     resolveFunction("Connection Established")
+//   } else {
+//     rejectFunction(Error("Connection Failed"));
+
+//   }
+// })
+
+// console.log(myPromise);
+
+// let resolver = (resolveValue) => console.log(`Good ${resolveValue}`);
+// let rejecter = (rejectValue) => console.log(`Bad ${rejectValue}`);
+
+// myPromise.then(resolver, rejecter)
+
+// myPromise.then(
+//   (resolveValue) => console.log(`Good ${resolveValue}`),
+//   (rejectValue) => console.log(`Bad ${rejectValue}`)
+// );
+
+
+/*
+  Promise Training
+
+  We Will Go To The Meeting, Promise Me That We Will Find The 4 Employees
+  .then(We Will Choose Two People)
+  .then(We Will Test Them Then Get One Of Them)
+  .catch(No One Came)
+
+  Then    => Promise Is Successfull Use The Resolved Data
+  Catch   => Promise Is Failed, Catch The Error
+  Finally => Promise Successfull Or Failed Finally Do Something
+*/
+
+// const myPromise = new Promise((resolveFunction, rejectFunction) => {
+//   let employees = [];
+//   if (employees.length === 4) {
+//     resolveFunction(employees);
+//   } else {
+//     rejectFunction(Error("Number Of Employees Is Not 4"));
+//   }
+// });
+
+// myPromise.then((resolveValue) => {
+//   resolveValue.length = 2
+//   return resolveValue
+// }).then((resolveValue) => {
+//   resolveValue.length = 1
+//   return resolveValue
+// }).then((resolveValue) => {
+//   console.log(resolveValue);
+// }).catch((rejectedReason) => { console.log(rejectedReason) })
+//   .finally(console.log("The Operation Is Done"));
+
+
+/*
+  Promise And XHR
+*/
+// const getData = (apiLink) => {
+//   return new Promise((resolve, reject) => {
+//     let myReq = new XMLHttpRequest();
+
+//     myReq.onload = function () {
+//       if (this.readyState === 4 && this.status === 200) {
+//         resolve(JSON.parse(this.responseText))
+//       } else {
+//         reject(Error("No Data Found"));
+//       }
+//     }
+//     myReq.open("GET", apiLink)
+//     myReq.send()
+//   })
+// }
+// getData("https://api.github.com/users/klay66/repos")
+//   .then((res) => {
+//     res.length = 10
+//     return res
+//   }).then((res) => {
+//     console.log(res[0].name);
+//   }).catch((rej) => console.log(rej));
+
+
+/*
+  Fetch API
+  - Return A Representation Of the Entire HTTP Response
+*/
+
+// fetch("https://api.github.com/users/klay66/repos")
+//   .then((res) => {
+//     console.log(res);
+//     let myData = res.json()
+//     console.log(myData)
+//     return myData
+//   })
+//   .then((val) => {
+//     val.length = 10
+//     return val
+//   })
+//   .then((log) => {
+//     console.log(log[0].name);
+
+//   })
+
+
+/*
+  Promise
+  - All
+  - All Settled
+  - Race
+*/
+
+// const myFirstPromise = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res("Iam The First Promise");
+//   }, 5000);
+// });
+
+// const mySecondPromise = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res("Iam The Second Promise");
+//   }, 1000);
+// });
+
+// const myThirdPromise = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     rej("Iam The Third Promise");
+//   }, 2000);
+// });
+
+// Promise.all([myFirstPromise, mySecondPromise, myThirdPromise]).then(
+//   (resolvedValues) => console.log(resolvedValues),
+//   (rejectedValue) => console.log(`Rejected: ${rejectedValue}`)
+// );
+
+
+// Promise.allSettled([myFirstPromise, mySecondPromise, myThirdPromise]).then(
+//   (resolvedValues) => console.log(resolvedValues),
+//   (rejectedValue) => console.log(`Rejected: ${rejectedValue}`)
+// );
+
+// Promise.race([myFirstPromise, mySecondPromise, myThirdPromise]).then(
+//   (resolvedValues) => console.log(resolvedValues),
+//   (rejectedValue) => console.log(`Rejected: ${rejectedValue}`)
+// );
+
+
+/*
+  Async
+  - Async Before Function Mean This Function Return A Promise
+  - Async And Await Help In Creating Asynchronous Promise Behavior With Cleaner Style
+*/
+
+// function getData() {
+//   return new Promise((res, rej) => {
+//     let users = [];
+//     if (users.length > 0) {
+//       res("Users Found");
+//     } else {
+//       rej("No Users Found");
+//     }
+//   });
+// }
+
+// getData().then(
+//   (resolvedValue) => console.log(resolvedValue),
+//   (rejectedValue) => console.log("Rejected " + rejectedValue)
+// );
+
+
+// function getData() {
+//   let users = [];
+//   if (users.length > 0) {
+//     return Promise.resolve("Users Found");
+//   } else {
+//     return Promise.reject("No Users Found");
+//   }
+
+// }
+
+// getData().then(
+//   (resolvedValue) => console.log(resolvedValue),
+//   (rejectedValue) => console.log("Rejected " + rejectedValue)
+// );
+
+
+// async function getData() {
+//   let users = ["nour"];
+//   if (users.length > 0) {
+//     return "Users Found";
+//   } else {
+//     throw new Error("No Users Found");
+//   }
+// }
+
+// console.log(getData());
+
+// getData().then(
+//   (resolvedValue) => console.log(resolvedValue),
+//   (rejectedValue) => console.log("Rejected " + rejectedValue)
+// );
+
+
+/*
+  Await
+  - Await Works Only Inside Asnyc Functions
+  - Await Make JavaScript Wait For The Promise Result
+  - Await Is More Elegant Syntax Of Getting Promise Result
+*/
+
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Iam The Good Promise");
+//     // reject(Error("Iam The Bad Promise"));
+//   }, 3000);
+// });
+
+// async function readData() {
+//   console.log("Before Promise");
+//   // myPromise.then((resolvedValue) => console.log(resolvedValue));
+//   console.log(await myPromise);
+//   // console.log(await myPromise.catch((err) => err));
+//   console.log("After Promise");
+// }
+
+// readData();
+
+/*
+  Async & Await With Try, Catch, Finally
+*/
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Iam The Good Promise");
+  }, 3000);
+});
+
+// async function readData() {
+//   console.log("Before Promise");
+
+//   try {
+//     console.log(await myPromise);
+//   } catch (reason) {
+//     console.log(`Reason${reason}`);
+//   } finally {
+//     console.log("After Promise");
+//   }
+// }
+
+// readData();;
+
+
+async function fetchData() {
+  console.log("Before Promise");
+
+  try {
+    let myData = await fetch("https://api.github.com/users/klay66/repos")
+    console.log(await myData.json());
+  } catch (reason) {
+    console.log(`Reason${reason}`);
+  } finally {
+    console.log("After Promise");
+  }
+}
+
+fetchData()
